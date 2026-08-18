954f21ae1622e3c8cd6668cd3ca4e4f7071fc432
MaydayAlaska <43659965+MaydayAlaska@users.noreply.github.com>
2026-08-18 20:08:42 +0200

    diag: persist fatal application exceptions

--- src/LocalStoreManagement.Desktop/Program.cs
+++ src/LocalStoreManagement.Desktop/Program.cs
@@ -1,4 +1,5 @@
 using Avalonia;
+using LocalStoreManagement.Desktop.Infrastructure;
 
 namespace LocalStoreManagement.Desktop;
 
@@ -7,7 +8,29 @@ internal static class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        AppDomain.CurrentDomain.UnhandledException += (_, eventArgs) =>
+        {
+            if (eventArgs.ExceptionObject is Exception exception)
+            {
+                AppLog.Error($"Unhandled AppDomain exception (terminating={eventArgs.IsTerminating})", exception);
+            }
+        };
+
+        TaskScheduler.UnobservedTaskException += (_, eventArgs) =>
+        {
+            AppLog.Error("Unobserved task exception", eventArgs.Exception);
+            eventArgs.SetObserved();
+        };
+
+        try
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            AppLog.Error("Application terminated unexpectedly", ex);
+            throw;
+        }
     }
 
     public static AppBuilder BuildAvaloniaApp()
