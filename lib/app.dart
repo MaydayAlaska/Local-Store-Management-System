d2335be469da803ea8cf2ca495218e06e1928f0d
MaydayAlaska <43659965+MaydayAlaska@users.noreply.github.com>
2026-08-19 14:30:11 +0200

    feat: apply theme language and currency settings

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'core/app_runtime.dart';
 import 'models/app_settings.dart';
 import 'pages/shell_page.dart';
 import 'services/app_services.dart';
@@ -22,17 +24,29 @@ class _StoreAppState extends State<StoreApp> {
     _settings = widget.services.settings.load();
   }
 
+  ThemeMode get _themeMode => switch (_settings.themeMode) {
+        'light' => ThemeMode.light,
+        'dark' => ThemeMode.dark,
+        _ => ThemeMode.system,
+      };
+
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Local Store Management System',
-        themeMode: ThemeMode.system,
-        theme: GlassTheme.light(),
-        darkTheme: GlassTheme.dark(),
-        home: ShellPage(
-          services: widget.services,
-          settings: _settings,
-          onSettingsChanged: (settings) => setState(() => _settings = settings),
-        ),
-      );
+  Widget build(BuildContext context) {
+    AppRuntime.apply(_settings);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Local Store Management System',
+      themeMode: _themeMode,
+      theme: GlassTheme.light(),
+      darkTheme: GlassTheme.dark(),
+      locale: Locale(_settings.languageCode),
+      supportedLocales: const [Locale('it'), Locale('en')],
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      home: ShellPage(
+        services: widget.services,
+        settings: _settings,
+        onSettingsChanged: (settings) => setState(() => _settings = settings),
+      ),
+    );
+  }
 }
