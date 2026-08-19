11d3cdf5925f8475b028b7dfcfb944d44f93db3b
MaydayAlaska <43659965+MaydayAlaska@users.noreply.github.com>
2026-08-19 17:41:08 +0200

    test: align beta OTA version to b7

--- test/update_service_test.dart
+++ test/update_service_test.dart
@@ -5,7 +5,7 @@ import 'package:local_store_management/services/update_service.dart';
 
 void main() {
   test('current beta OTA version is aligned', () {
-    expect(UpdateService.currentVersion, '0.1.6-b6');
+    expect(UpdateService.currentVersion, '0.1.6-b7');
   });
 
   test('OTA selects Windows x64 asset', () {
@@ -111,10 +111,10 @@ void main() {
   });
 
   test('beta and stable OTA channels reject each other versions', () {
-    expect(versionBelongsToUpdateChannel('0.1.6-b6', beta: true), isTrue);
+    expect(versionBelongsToUpdateChannel('0.1.6-b7', beta: true), isTrue);
     expect(versionBelongsToUpdateChannel('0.1.6', beta: true), isFalse);
     expect(versionBelongsToUpdateChannel('0.1.6', beta: false), isTrue);
-    expect(versionBelongsToUpdateChannel('0.1.6-b6', beta: false), isFalse);
+    expect(versionBelongsToUpdateChannel('0.1.6-b7', beta: false), isFalse);
     expect(versionBelongsToUpdateChannel('not-a-version', beta: true), isFalse);
     expect(versionBelongsToUpdateChannel('not-a-version', beta: false), isFalse);
   });
