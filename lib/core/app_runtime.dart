2a0f8759476ea64c502baca1ad12d552492d2469
MaydayAlaska <43659965+MaydayAlaska@users.noreply.github.com>
2026-08-19 14:35:43 +0200

    feat: expose selected currency symbol

--- lib/core/app_runtime.dart
+++ lib/core/app_runtime.dart
@@ -4,6 +4,13 @@ class AppRuntime {
   static String currencyCode = AppSettings.defaults.currencyCode;
   static String languageCode = AppSettings.defaults.languageCode;
 
+  static String get currencySymbol => switch (currencyCode) {
+        'USD' => r'$',
+        'GBP' => '£',
+        'CHF' => 'CHF',
+        _ => '€',
+      };
+
   static void apply(AppSettings settings) {
     currencyCode = settings.currencyCode;
     languageCode = settings.languageCode;
